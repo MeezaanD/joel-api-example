34ce74d9de664956a1b3c323e1baac6414f6b1f8
Meezaan Davids <meezaandavids365@gamil.com>
2023-02-21 16:37:28 +0200

    second

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require('express');
+// route
+const route = require('./controller');
+// cors
 const cors = require('cors');
-// const bodyParser = require('body-parser');
-
+// port 
+const port = parseInt(process.env.PORT) || 2000;
+// Express app
 const app = express();
+// Middleware
+const {errorHandling} = require('./middleware/ErrorHandling');
+//
+const cookieParser = require('cookie-parser');
+/*
+express.json: setting the content-type to application/json
+bodyParser.urlencoded( {extended: true} ): Object will contain
+values of any type instead of just a string
+*/
+app.use((req, res, next)=> {
+        res.header('Access-Control-Allow-Origin', 'http://localhost:2000')
+        res.header("Access-Control-Allow-Credentials", "true")
+        res.header("Access-Control-Allow-Methods", "*")
+        res.header("Access-Control-Allow-Headers", "*")
+        next();
+});
+app.use(route);
+app.use(
+    cors(),
+    cookieParser(),
+    express.json,
+    express.urlencoded({extended: false})
+)
 
-var corsOptions  = {
-    origin: "http://localhost:2000"
-};
-
-app.use(cors(corsOptions));
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true}));
-
+// Server is running
+app.listen(port, ()=> {
+    console.log(`Server is running at http://localhost:2000 `)
+});
+// Handling all errors
+app.use(errorHandling);
