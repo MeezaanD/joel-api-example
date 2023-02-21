3d2a2dab83d29b34430a3b323b475f12c0e24a44
Meezaan Davids <meezaandavids365@gamil.com>
2023-02-21 16:11:52 +0200

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+// const bodyParser = require('body-parser');
+
+const app = express();
+
+var corsOptions  = {
+    origin: "http://localhost:2000"
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true}));
+
